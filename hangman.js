d88ef2a9a2c92b58ace5c2e52398d77b67b02915
James Hawker <526rserbinenko@frhsd.com>
2025-03-20 17:48:17 +0000

    working on the Game class and animation stabilisation

--- hangman.js
+++ hangman.js
@@ -1,4 +1,57 @@
-function start(mode) {}
+const words = [
+  "dyke",
+  "abyss",
+  "swamp",
+  "moss",
+  "pub", //five or lower
+  "whiskey",
+  "quirky",
+  "lettuce",
+  "quantum",
+  "icefall", //seven or lower
+  "mountain",
+  "sabotage",
+  "jackhammer",
+  "leprechaun",
+  "tablecloth", //10 or lower
+  "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu",
+  "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch",
+  "Chargoggaggoggmanchauggaggoggcharbunugungamaugg",
+  "Pneumonoultramicroscopicsilicovolcanoconiosis",
+  "Hippopotomonstrosesquipedaliaphobia", //everything else
+];
+function start(mode) {
+  document.querySelector(".opening-text").classList.add("d-none");
+  document.querySelector(".opening-card").classList.add("open-card-anim");
+  setTimeout(
+    () => {
+      document
+        .querySelector(".opening-card")
+        .classList.add("open-card-anim-done");
+      document.getElementsByTagName("STYLE")[0].append(`
+        .openedCard::after, .openedCard::before {
+        width: ${document.querySelector(".opening-card").offsetWidth * 1.8}px;
+        height: ${document.querySelector(".opening-card").offsetHeight * 2.5}px;
+        transform: scaleX(1) scaleY(0);
+        }  
+      `);
+      document.querySelector(".opening-card").classList.add(".openedCard");
+      document
+        .querySelector(".opening-card")
+        .classList.remove(".open-card-anim");
+    },
+    4000,
+    document
+  );
+  ///////////////
+  let selection = words.filter((val) => {
+    if (mode == 1) return val.length <= 5;
+    if (mode == 2) return val.length > 5 && val.length <= 7;
+    if (mode == 3) return val.length > 7 && val.length <= 10;
+    if (mode == 4) return val.length > 10;
+  });
+  let game = new Game(Math.floor(Math.random() * selection.length));
+}
 function modeButtons() {
   document.querySelector(".button").classList.add("d-none");
   document
@@ -8,7 +61,6 @@ function modeButtons() {
 
 class Game {
   word;
-  mode;
   constructor(word, mode) {
     this.word = word.split("");
     this.mode = mode;
@@ -41,16 +93,19 @@ class Game {
       "z",
     ];
   }
-  guess(arg) {}
+  init() {
+    let container = document.querySelector(".wordCont");
+    for (let i = 0; i < this.word.length; i++) {
+      let div = document.createElement("div");
+      div.id = `letter_${i}`;
+      container.append(div);
+    }
+  }
+  guess(arg) {
+    if (this.word.includes(arg)) {
+      this.word.forEach((val, ind) => {
+        document.querySelector(`#letter_${ind}`).textContent = val;
+      });
+    }
+  }
 }
-
-/*document.querySelector(".opening-card").classList.add("open-card-anim");
-  setTimeout(
-    () => {
-      document
-        .querySelector(".opening-card")
-        .classList.add("open-card-anim-done");
-    },
-    5000,
-    document
-  ); */
